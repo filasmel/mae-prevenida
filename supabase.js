173d278b0c76e5a221d3973e45f4fd21223a0335
filasmel <filomeno1989a@gmail.com>
2026-06-12 16:18:02 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,7 @@
+// Aguarda o SDK carregar antes de inicializar
+const { createClient } = window.supabase;
+
 const SUPABASE_URL = 'https://ruwhxvckbcomlnysrvzv.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1d2h4dmNrYmNvbWxueXNydnp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExNjE1MzgsImV4cCI6MjA5NjczNzUzOH0.Ah4EI31JZTs9AmISW6nMlJ2VUO0vKbRg3JyWukp4o0Y';
 
-const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
